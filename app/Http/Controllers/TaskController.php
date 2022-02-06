fa7a1d5ed6aac2cd2d395b2923c0da13113f545e
Obeidareda37 <obeidareda37@gmail.com>
2022-02-06 20:27:03 +0200

    TDD CRUD laravel

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
+use App\Http\Resources\TaskResource;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return TaskResource::collection(Task::paginate());
+    }
+
+    public function store(StoreTaskRequest $request)
+    {
+        $task = Task::create($request->validated());
+
+        return new TaskResource($task);
+    }
+
+    public function update(UpdateTaskRequest $request, Task $task)
+    {
+        $task->update($request->validated());
+
+        return new TaskResource($task);
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+    }
+}
---
+++ tests/Feature/TaskControllerTest.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Database\Factories\TaskFactory;
+use Tests\TestCase;
+
+class TaskControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_can_get_all_tasks()
+    {
+        TaskFactory::new()->count(20)->create();
+
+        $this->getJson('api/tasks')
+            ->assertJsonStructure([
+                'data' => [
+                    [
+                        'id',
+                        'title',
+                        'status',
+                    ],
+                ],
+            ])
+            ->assertJsonCount(15, 'data')
+            ->assertJson([
+                'meta' => [
+                    'total' => 20,
+                ],
+            ]);
+    }
+
+    public function test_can_create_new_tasks()
+    {
+        $this->postJson('api/tasks', [
+            'title' => "example task",
+        ])->assertSuccessful()
+            ->assertJson([
+                'data' => [
+                    'id' => 1,
+                    'title' => 'example task',
+                    'status' => false,
+                ]
+            ]);
+    }
+
+    public function test_task_is_incompleted_by_default()
+    {
+        $this->postJson('api/tasks', [
+            'title' => "example task",
+            'status' => true,
+        ])->assertSuccessful()
+            ->assertJson([
+                'data' => [
+                    'id' => 1,
+                    'title' => 'example task',
+                    'status' => false,
+                ]
+            ]);
+    }
+
+    public function test_task_title_is_requiered()
+    {
+        $this->postJson('api/tasks', [
+            'title1' => "example task",
+        ])->assertJsonValidationErrorFor('title');
+    }
+
+    public function test_can_update_current_task()
+    {
+        TaskFactory::new()->create([
+            'title' => 'old title',
+            'description'=>'hhhh',
+            'status' => false,
+        ]);
+
+        $this->putJson('api/tasks/1', [
+            'title' => "new title",
+            'description'=>'hhhh',
+            'status' => true,
+        ])->assertSuccessful()
+            ->assertJson([
+                'data' => [
+                    'id' => 1,
+                    'title' => 'new title',
+                    'description'=>'hhhh',
+                    'status' => true,
+                ]
+            ]);
+    }
+
+    public function test_can_update_completed_task()
+    {
+        TaskFactory::new()->create([
+            'title' => 'old title',
+            'status' => true,
+        ]);
+
+        $this->putJson('api/tasks/1', [
+            'title' => "new title",
+            'status' => false,
+        ])->assertSuccessful()
+            ->assertJson([
+                'data' => [
+                    'id' => 1,
+                    'title' => 'new title',
+                    'status' => false,
+                ]
+            ]);
+    }
+
+    public function test_can_delete_current_task()
+    {
+        TaskFactory::new()->create([
+            'title' => 'old title',
+            'status' => false,
+        ]);
+
+        $this->deleteJson('api/tasks/1')
+            ->assertSuccessful();
+
+        $this->assertDatabaseCount('tasks', 0);
+    }
+}
